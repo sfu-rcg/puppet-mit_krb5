cf63d57d52b169030e9a3a4957c39a6d03218bf3
Derek Warren <warren@sfu.ca>
2015-03-09 15:34:18 -0700

    Revert "Give up on Hiera"
    This reverts commit 733cb61cdc695cbbe43356d4b3ec7b28a64f0620.

--- manifests/domain_realms.pp
+++ manifests/domain_realms.pp
@@ -1,7 +1,7 @@
 
 class mit_krb5::domain_realms(
   $domains = {},
-  $defaults = hiera(mit_krb5::domain_realms),
+  $defaults = {},
   ) {
   create_resources(domain_realm, $domains, $defaults)
 }
--- manifests/realms.pp
+++ manifests/realms.pp
@@ -1,7 +1,7 @@
 
 class mit_krb5::realms(
   $realms = {},
-  $defaults = hiera(mit_krb5::realms),
+  $defaults = {},
   ) {
   create_resources(realm, $realms, $defaults)
 }
