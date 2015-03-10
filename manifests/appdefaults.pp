a349eb86c0f3ffbfe4f4a010961c91df90237ac8
Derek Warren <warren@sfu.ca>
2015-03-10 13:15:09 -0700

    Allow Foreman to manage appdefault type

--- manifests/appdefaults.pp
+++ manifests/appdefaults.pp
@@ -1,37 +1,7 @@
-# == Define: mit_krb5::appdefaults
-#
-# Configure appdefaults section of krb5.conf
-#
-# === Authors
-#
-# Patrick Mooney <patrick.f.mooney@gmail.com>
-#
-# === Copyright
-#
-# Copyright 2013 Patrick Mooney.
-#
 
-/* learning about concat module and Puppet 3 at the same time
-   so you almost certainly should not use this */
-
-define mit_krb5::appdefaults(
-  $app            = $title,
-  $content        = {} ,
+class mit_krb5::appdefaultss(
+  $apps = {},
+  $defaults = {},
   ) {
-
-  include mit_krb5
-  
-  validate_hash($content)
-  $_settings = $content
-
-  ensure_resource('concat::fragment', 'mit_krb5::appdefaults_header', {
-    target  => $mit_krb5::krb5_conf_path,
-    order   => '30appdefaults_header',
-    content => "[appdefaults]\n",
-  })
-    concat::fragment { "mit_krb5::appdefaults::${app}":
-      target  => $mit_krb5::krb5_conf_path,
-      order   => "31appdefault::${app}",
-      content => template('mit_krb5/appdefaults.erb'),
-    }
-  }
+  create_resources(appdefault, $apps, $defaults)
+}
